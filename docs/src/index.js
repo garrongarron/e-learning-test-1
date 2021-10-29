5ee39e9ae8118cae39949dff3c1104ff85310d1d
Samu Garron <garron.garron@gmail.com>
2021-10-28 22:09:21 -0300

    Ading hack to run in a subfolder in github pages

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,18 +1,6 @@
 import cssLoader from "./basic/CssLoader.js";
 import Routing from "./basic/Routing.js";
 
-let addBase = (host) =>{
-    let base = document.head.querySelector('base')
-    base = (!base)?document.createElement('base'):base
-    base.href = host
-    document.head.appendChild(base)
-}
-if(location.origin == 'https://garrongarron.github.io'){
-    addBase('https://garrongarron.github.io/e-learning-test-1/')
-} else{
-    addBase(location.origin)
-}
-
 let folder = 'src/css/'
 let cssList = [
     'font.css',
